000bce21420b8ba1f22dcce37df4eb365347c2e6
mzw <zhenwei.ma@siycrm.com>
2022-05-06 16:52:30 +0800

    初步实现

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-
 // const urls = [
 //     {
 //         url: 'https://ss.cc',
 //         enable: true
 //     }
 // ]
+
+function genRule(urlInfo, id) {
+    const { url } = urlInfo
+    return {
+        "id": id,
+        "priority": 2,
+        "action": {
+            "type": "modifyHeaders",
+            "responseHeaders": [
+                {"header": "Access-Control-Allow-Origin", "operation": "set", "value": "*"},
+            ]
+        },
+        "condition": {
+            "urlFilter": url,
+            "resourceTypes": ["image", "main_frame", "stylesheet", "script", "other", "xmlhttprequest"]
+        }
+    }
+}
 
 const URLS = 'urls'
 const app = document.getElementById('app')
@@ -14,56 +31,50 @@ const saveBtn = document.getElementById('saveBtn')
 const addBlockUrlInput = document.getElementById('addBlockUrlInput')
 addRule.addEventListener('click', (e) => {
     addUrlForm.style.display = 'block'
-    // chrome.declarativeNetRequest.updateDynamicRules({
-    //     removeRuleIds: [14],
-    //     addRules: [{
-    //         "id": 14,
-    //         "priority": 2,
-    //         "action": {
-    //             "type": "modifyHeaders",
-    //             "responseHeaders": [
-    //                 { "header": "Access-Control-Allow-Origin", "operation": "set", "value": "*" },
-    //                 { "header": "bb", "operation": "set", "value": "*1" }
-    //             ]
-    //         },
-    //         "condition": {
-    //             "urlFilter": "*",
-    //             "resourceTypes": ["image", "main_frame", "stylesheet", "script", "other", "xmlhttprequest"]
-    //         }
-    //     }]
-    // }, function (res) {
-    //     console.log(res)
-    // })
 })
 
 saveBtn.addEventListener('click', function handleSave() {
     console.log('addBlockUrlInput', addBlockUrlInput.value)
-    chrome.storage.local.get(URLS, function(result) {
+    chrome.storage.local.get(URLS, function (result) {
         const urls = result[URLS] || []
-        chrome.storage.local.set({[URLS]: [...urls, { url: addBlockUrlInput.value, enable: true}]}, function() {
+        const newUrls = [...urls, {url: addBlockUrlInput.value, enable: true}]
+        chrome.declarativeNetRequest.updateDynamicRules({
+            removeRuleIds: Array(urls.length).fill(0).map((_,i) => i + 1),
+            addRules: newUrls.filter(urlInfo => urlInfo.enable).map((item, index) => genRule(item, index + 1))
+        })
+        chrome.storage.local.set({[URLS]: newUrls}, function () {
             renderList()
         });
     });
     addUrlForm.style.display = 'none'
 })
 
-app.addEventListener('click', function (e) {
+app.addEventListener('click', function handleAppClick(e) {
     const targetElementId = e.target.id
     console.log('click', targetElementId)
     if (targetElementId?.indexOf("checkbox") !== -1) {
         const checkboxIndex = Number(targetElementId.split('-').pop())
-        chrome.storage.local.get(URLS, function(result) {
+        chrome.storage.local.get(URLS, function (result) {
             const urls = result[URLS]
-            console.log(JSON.stringify(urls), JSON.stringify(urls.map((urlInfo, index) => index === checkboxIndex ? {...urlInfo, enable: !urls[index].enable} : urlInfo)))
-            chrome.storage.local.set({[URLS]: urls.map((urlInfo, index) => index === checkboxIndex ? {...urlInfo, enable: !urls[index].enable} : urlInfo)}, function() {
+            const newUrls = urls.map((urlInfo, index) => index === checkboxIndex ? {
+                ...urlInfo,
+                enable: !urls[index].enable
+            } : urlInfo)
+            chrome.declarativeNetRequest.updateDynamicRules({
+                removeRuleIds: Array(urls.length).fill(0).map((_,i) => i + 1),
+                addRules: newUrls.filter(urlInfo => urlInfo.enable).map((item, index) => genRule(item, index + 1))
+            })
+            chrome.storage.local.set({
+                [URLS]: newUrls
+            }, function () {
                 renderList()
             });
         });
     }
 })
 
-function renderList () {
-    chrome.storage.local.get(URLS, function(result) {
+function renderList() {
+    chrome.storage.local.get(URLS, function (result) {
         console.log('Value currently is ', result[URLS]);
         const urls = result[URLS]
         let str = ''
@@ -76,20 +87,7 @@ function renderList () {
         app.innerHTML = str
     });
 }
+
 window.onload = () => {
     renderList()
 }
-// blockUrls.forEach((domain, index) => {
-//     let id = index + 1;
-//
-//     chrome.declarativeNetRequest.updateDynamicRules(
-//         {addRules:[{
-//                 "id": id,
-//                 "priority": 1,
-//                 "action": { "type": "block" },
-//                 "condition": {"urlFilter": domain, "resourceTypes": ["main_frame"] }}
-//             ],
-//             removeRuleIds: [id]
-//         },
-//     )
-// })
