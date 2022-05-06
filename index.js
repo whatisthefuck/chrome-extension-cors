4d994ed466f8b553e1ddebe1ae99f02fa0acb140
mzw <zhenwei.ma@siycrm.com>
2022-05-06 17:12:23 +0800

    初步完成

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function genRule(urlInfo, id) {
 }
 
 const URLS = 'urls'
-const app = document.getElementById('app')
+const listWrap = document.getElementById('listWrap')
 const addRule = document.getElementById('addRule')
 const addUrlForm = document.getElementById('addUrlForm')
 const saveBtn = document.getElementById('saveBtn')
@@ -49,10 +49,11 @@ saveBtn.addEventListener('click', function handleSave() {
     addUrlForm.style.display = 'none'
 })
 
-app.addEventListener('click', function handleAppClick(e) {
+listWrap.addEventListener('click', function handleListWrapClick(e) {
     const targetElementId = e.target.id
     console.log('click', targetElementId)
     if (targetElementId?.indexOf("checkbox") !== -1) {
+        // handleCheckboxClick
         const checkboxIndex = Number(targetElementId.split('-').pop())
         chrome.storage.local.get(URLS, function (result) {
             const urls = result[URLS]
@@ -60,6 +61,22 @@ app.addEventListener('click', function handleAppClick(e) {
                 ...urlInfo,
                 enable: !urls[index].enable
             } : urlInfo)
+            chrome.declarativeNetRequest.updateDynamicRules({
+                removeRuleIds: Array(urls.length).fill(0).map((_,i) => i + 1),
+                addRules: newUrls.filter(urlInfo => urlInfo.enable).map((item, index) => genRule(item, index + 1))
+            })
+            chrome.storage.local.set({
+                [URLS]: newUrls
+            }, function () {
+                renderList()
+            });
+        });
+    } else if (targetElementId?.indexOf("delBtn") !== -1) {
+        // handleDelBtnClick
+        const delBtnIndex = Number(targetElementId.split('-').pop())
+        chrome.storage.local.get(URLS, function (result) {
+            const urls = result[URLS]
+            const newUrls = urls.filter((urlInfo, index) => index !== delBtnIndex)
             chrome.declarativeNetRequest.updateDynamicRules({
                 removeRuleIds: Array(urls.length).fill(0).map((_,i) => i + 1),
                 addRules: newUrls.filter(urlInfo => urlInfo.enable).map((item, index) => genRule(item, index + 1))
@@ -79,12 +96,13 @@ function renderList() {
         const urls = result[URLS]
         let str = ''
         urls?.forEach((urlInfo, index) => {
-            str += `<div style="display: flex">
+            str += `<div class="flex item">
                        <input type=checkbox ${urlInfo.enable && 'checked'} id=checkbox-${index} />
-                       <div>${urlInfo.url}</div>
+                       <div style="flex: 1">${urlInfo.url}</div>
+                       <div class="delBtn" id=delBtn-${index}>删除</div>
                     </div>`
         })
-        app.innerHTML = str
+        listWrap.innerHTML = str
     });
 }
 
